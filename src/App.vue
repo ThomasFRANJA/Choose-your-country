6cf64f492381597d1582e4a6fb3dd70f150dbbc4
ThomasFranja <thomas.franja@firmfunding.com>
2020-03-12 12:58:15 +0100

    [Front](feats): #criterion n°1, n°2 => Clicking on the text field, a drop-down menu is displayed with the list of possible country choices. The drop-down menu is scrollable

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   section.countries
       .container-box
-        input(v-model="message" placeholder="Saisissez votre pays" label="Pays")
-        //- p Votre pays est : {{message}}
+        label Pays
+        input(v-model="search" @click="onChange" placeholder="Saisissez votre pays" label="Pays")
+        //- p Votre pays est : {{search}}
 
-        ul.container-box-content
+        ul.container-box-content(v-if="showModal")
           li(v-for="item in countries" :key="item.name").container-box-content__list
             img.container-box-content__img(:src="item.flag" alt="flag of this countrie")
             span.container-box-content__name {{ item.name }}
+
+      div(style="margin-top: 50px")
+        span {{`Votre pays choisi est: ${this.search}`}}
 
 
 </template>
@@ -28,27 +32,11 @@ export default {
     Autocomplete,
   },
 
-  methods: {
-    // 
-  },
-
-  mounted() {
-    Vue.axios.get('https://restcountries.eu/rest/v2/all')
-      .then((res) => {
-      this.countries = res.data;
-      console.log(res.data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  },
-
-
-
   data() {
     return {
       countries: [],
-      message: '',
+      search: '',
+      showModal: false,
       items: [
         {
           id: 0,
@@ -80,7 +68,32 @@ export default {
         },
       ]
     }
-  }
+  },
+
+  methods: {
+    // 
+    onChange() {
+      // 
+      this.showModal = true 
+      console.log('Votre texte est:', this.search)
+    } 
+  },
+
+  mounted() {
+    // Here, i get the api to the mounted of project for display result the countries
+    Vue.axios.get('https://restcountries.eu/rest/v2/all')
+      .then((res) => {
+      // Here, i will be push and assign the response result countries in the data array initialized to top
+      this.countries = res.data;
+      // console.log(res.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  },
+
+
+
 
 };
 </script>
@@ -102,15 +115,17 @@ export default {
     input {
       outline: none;
       font-size: 1rem;
-      border-bottom: 1px solid;
+      border-bottom: 2px solid;
       border-color: #94bbdc;
       width: 100%;
+      font-family: "Campton-BoldDEMO";
     }
     &-content {
+      background-color: #fff;
       border-radius: 3px;
       height: 130px;
       position: absolute;
-      top: 50px;
+      top: 70px;
       margin: 0 25px;
       left: 0;
       right: 0;
@@ -138,3 +153,5 @@ export default {
 }
   
 </style>
+
+
