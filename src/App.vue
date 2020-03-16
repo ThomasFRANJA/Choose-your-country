6bde877ad67374f1b2c89ecb8b083fc4732e50ff
ThomasFranja <thomas.franja@firmfunding.com>
2020-03-16 09:29:11 +0100

    [Front](feats): criterion #4 => Si l’utilisateur clique sur un pays, le nom du pays est renseigné dans le champ texte et le menu déroulant est fermé; criterion #5 Une fois une valeur sélectionnée l’attribut « v-model » du composant doit renvoyer le code ISO du pays

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template lang="pug">
   section.countries
-      .container-box
-        label Pays
-        input(v-model="search" @click="onChange" placeholder="Saisissez votre pays" label="Pays")
-        //- p Votre pays est : {{search}}
-
-        ul.container-box-content(v-if="showModal")
-          li(v-for="item in countries" :key="item.name").container-box-content__list
-            img.container-box-content__img(:src="item.flag" alt="flag of this countrie")
-            span.container-box-content__name {{ item.name }}
-
-      div(style="margin-top: 50px")
-        span {{`Votre pays choisi est: ${this.search}`}}
+    <Autocomplete/>
 
 
 </template>
 
 <script>
 import Autocomplete from './components/Autocomplete';
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-Vue.use(VueAxios, axios)
 
 
 export default {
@@ -33,68 +17,7 @@ export default {
   },
 
   data() {
-    return {
-      countries: [],
-      search: '',
-      showModal: false,
-      items: [
-        {
-          id: 0,
-          countrie: "Allemagne"
-        },
-        {
-          id: 1,
-          countrie: "Brésil"
-        },
-        {
-          id: 2,
-          countrie: "Croatie"
-        },
-        {
-          id: 3,
-          countrie: "Danemark"
-        },
-        {
-          id: 4,
-          countrie: "Espagne"
-        },
-        {
-          id: 5,
-          countrie: "France"
-        },
-        {
-          id: 6,
-          countrie: "Germany"
-        },
-      ]
-    }
   },
-
-  methods: {
-    // 
-    onChange() {
-      // 
-      this.showModal = true 
-      console.log('Votre texte est:', this.search)
-    } 
-  },
-
-  mounted() {
-    // Here, i get the api to the mounted of project for display result the countries
-    Vue.axios.get('https://restcountries.eu/rest/v2/all')
-      .then((res) => {
-      // Here, i will be push and assign the response result countries in the data array initialized to top
-      this.countries = res.data;
-      // console.log(res.data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  },
-
-
-
-
 };
 </script>
 
@@ -104,52 +27,6 @@ export default {
   height: 100vh;
   display: flex;
   justify-content: center;
-  & .container-box {
-    width: 300px;
-    padding: 25px;
-    background: #fff;
-    border-radius: 3px;
-    position: relative;
-    height: 150px;
-    margin-top: 50px;
-    input {
-      outline: none;
-      font-size: 1rem;
-      border-bottom: 2px solid;
-      border-color: #94bbdc;
-      width: 100%;
-      font-family: "Campton-BoldDEMO";
-    }
-    &-content {
-      background-color: #fff;
-      border-radius: 3px;
-      height: 130px;
-      position: absolute;
-      top: 70px;
-      margin: 0 25px;
-      left: 0;
-      right: 0;
-      overflow: hidden;
-      overflow-y: scroll;
-      box-shadow: 0 0 8px 0px #94bbdc;
-      &__list {
-        display: flex;
-        align-content: center;
-        text-decoration-style: none;
-        padding: 15px;
-        &:hover {
-          background-color: #94badc28;
-          cursor: pointer;
-        }
-      }
-      &__img {
-        width: 25px;
-      }
-      &__name {
-        margin-left: 15px;
-      }
-    }
-  }
 }
   
 </style>
--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,73 +1,129 @@
 <template lang="pug">
-  v-autocomplete(
-    label="Pays"
-    :color="color"
-    :placeholder="placeholder"
-    v-model="select"
-    :loading="loading"
-    :items="items"
-    :search-input.sync="search"
-    cache-items
-    class="mx-4"
-    flat
-    hide-no-data
-    hide-details
-    solo-inverted
-  )
+.container-box
+  label Pays
+  input(v-model="search" @click="onChange"  @keyup.up="onChangeKeyUp" @keyup.down="onChangeKeyDown" placeholder="Saisissez votre pays" label="Pays")
+
+  ul.container-box-content(v-if="showModal")
+    li(v-for="item in countries" :key="item.name" @click="addCountrie(item.name)").container-box-content__list
+      img.container-box-content__img(:src="item.flag" alt="flag of this countrie")
+      span.container-box-content__name {{ item.name }}
 </template>
 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
   export default {
     name: 'Autocomplete',
 
   data: () => ({
-    // Initialized datas items states
-      loading: false,
-      items: [],
-      search: null,
-      select: null,
-      states: [
-        'Alabama',
-        'Alaska',
-        'American Samoa',
-        'Arizona',
-        'Arkansas',
-        'California',
-        'Colorado',
-        'Connecticut',
-        'Delaware',
-        'District of Columbia',
-      ],
+    // Initialized datas
+      countries: [],
+      countriesName: [],
+      search: '',
+      isLoading: false,
+      showModal: false,
+      counter: 0,
   }),
 
-  watch: {
-    search (val) {
-      val && val !== this.select && this.querySelections(val)
-    }
-  },
-    
   methods: {
-    querySelections (v) {
-      this.loading = true
-      // Simulated ajax query
-      setTimeout(() => {
-        this.items = this.states.filter(e => {
-          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
-        })
-        this.loading = false
-      }, 500)
+    onChange() {
+      this.$emit("input", this.search);
+      this.showModal = true
+      // console.log('Votre texte est:', this.search)
+      console.log('Votre element est:', this.filterResults())
+      // console.log('keydown clicked', this.onChangeKeyDown())
+      // console.log('data', this.countries)
+    },
+    filterResults() {
+      // 
+      this.countries.map(item => {
+        this.countriesName = item.name
+        console.log(this.countriesName)
+      })
+      },
+
+    addCountrie(item) {
+      this.search = item,
+      this.showModal = false
+      console.log(this.search)
+    },
+
+    onChangeKeyDown() {
+      if(this.counter < this.countries.length) {
+        this.counter = this.counter + 1
+      }
+    },
+
+    onChangeKeyUp() {
+      console.log('keyup clicked', this.counter)
     },
   },
 
-    props: {
-      color: {
-        type: String,
-        default: "#94bbdc"
-      },
-      placeholder: {
-        type: String,
-        default: "Entrez le nom de votre pays"
-      },
+  mounted() {
+    // Here, i get the api to the mounted of project for display result the countries
+      Vue.axios.get('https://restcountries.eu/rest/v2/all')
+        .then((res) => {
+        // Here, i will be push and assign the response result countries in the data array initialized to top
+        this.countries = res.data;
+        // console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
+
+<style lang="scss">
+.container-box {
+  width: 300px;
+  padding: 25px;
+  background: #fff;
+  border-radius: 3px;
+  position: relative;
+  height: 150px;
+  margin-top: 50px;
+  input {
+    outline: none;
+    font-size: 1rem;
+    border-bottom: 2px solid;
+    border-color: #94bbdc;
+    width: 100%;
+    font-family: "Campton-BoldDEMO";
+    position: relative;
+    margin-top: 10px;
+  }
+  &-content {
+    background-color: #fff;
+    border-radius: 3px;
+    height: 130px;
+    position: absolute;
+    margin: 0 25px;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+    overflow-y: scroll;
+    box-shadow: 0 0 8px 0px #94bbdc;
+    &__list {
+      display: flex;
+      align-content: center;
+      text-decoration-style: none;
+      padding: 15px;
+      &:hover {
+        background-color: #94badc28;
+        cursor: pointer;
+      }
+    }
+    &__img {
+      width: 25px;
+    }
+    &__name {
+      margin-left: 15px;
     }
   }
-</script>
+}
+</style>
